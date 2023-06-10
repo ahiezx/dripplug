5e36c93c4fe961268214158966e52c52a7e23470
blazewtt <ahmad.ahmapro2@gmail.com>
2023-06-10 17:28:30 +0200

    usefetch and format documents

--- app/page.tsx
+++ app/page.tsx
@@ -3,81 +3,89 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-    <main className="flex flex-col items-center justify-center w-full h-screen py-20 text-center bg-gradient-to-b bg-green-900">
-      <div className="animate-pulse">
-        <img
-          src="/logo.png"
-          alt="Drip Plug Logo"
-          width={300}
-          height={300}
-          className="select-none"
-        />
-      </div>
+      <main className="flex flex-col items-center justify-center w-full h-screen py-20 text-center bg-gradient-to-b bg-green-900">
+        <div className="animate-pulse">
+          <img
+            src="/logo.png"
+            alt="Drip Plug Logo"
+            width={300}
+            height={300}
+            className="select-none"
+          />
+        </div>
 
-      <p className="text-3xl text-white mt-10">
-        Welcome To Drip Plug. <br /> Let's get you dripped up 🤑
-      </p>
+        <p className="text-3xl text-white mt-10">
+          Welcome To Drip Plug. <br /> Let's get you dripped up 🤑
+        </p>
 
-      <a
-        href="search"
-        className="bg-green-500 hover:bg-green-700 text-white px-8 py-3 rounded-full font-bold transition duration-300 ease-in-out mt-10"
-      >
-        🔍 Search Products
-      </a>
-
-      <p className="mt-10 text-gray-300">Check us out!</p>
-      <i
-        id="arrow"
-        className="animate-bounce fad fa-arrow-down text-2xl text-white mt-3"
-      ></i>
-    </main>
-      <section>
-        <div className="flex flex-col items-center justify-center w-full py-10 text-center"
-        style={
-          {
-            background: "linear-gradient(90deg, rgba(92,68,158,0.40015756302521013) 0%, rgba(100,255,88,0.4049194677871149) 39%, rgba(30,156,148,0.45253851540616246) 100%);",
-          }
-        }
+        <a
+          href="search"
+          className="bg-green-500 hover:bg-green-700 text-white px-8 py-3 rounded-full font-bold transition duration-300 ease-in-out mt-10"
         >
-<h1 class="text-4xl font-bold mb-3 mt-5 text-white">Our Mission</h1>
+          🔍 Search Products
+        </a>
 
-<div class="flex flex-col md:flex-row items-center justify-center w-full py-10 text-center">
-  <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/3">
-    <i class="fad fa-globe-americas text-6xl text-green-500 mb-5"></i>
-    <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Quality</h1>
-    <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
-      We only work with the best manufacturers and designers in China to ensure that our customers receive the highest quality products.
-    </p>
-  </div>
-  <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/3">
-    <i class="fad fa-dollar-sign text-6xl text-green-500 mb-5"></i>
-    <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Affordability</h1>
-    <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
-      We work directly with manufacturers and designers to cut out the middleman and provide our customers with the best prices.
-    </p>
-  </div>
-  <div class="flex flex-col items-center justify-center md:w-1/3">
-    <i class="fad fa-headset text-6xl text-green-500 mb-5"></i>
-    <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Customer Service</h1>
-    <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
-      We are dedicated to providing our customers with the best service possible. If you have any questions or concerns, please contact us at
-      <a href="mailto:info@dripplug.io" class="text-green-500">info@dripplug.io</a>.
-    </p>
-  </div>
-</div>
+        <p className="mt-10 text-gray-300">Check us out!</p>
+        <i
+          id="arrow"
+          className="animate-bounce fad fa-arrow-down text-2xl text-white mt-3"
+        ></i>
+      </main>
+      <section>
+        <div
+          className="flex flex-col items-center justify-center w-full py-10 text-center"
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(92,68,158,0.40015756302521013) 0%, rgba(100,255,88,0.4049194677871149) 39%, rgba(30,156,148,0.45253851540616246) 100%);",
+          }}
+        >
+          <h1 class="text-4xl font-bold mb-3 mt-5 text-white">Our Mission</h1>
 
+          <div class="flex flex-col md:flex-row items-center justify-center w-full py-10 text-center">
+            <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/3">
+              <i class="fad fa-globe-americas text-6xl text-green-500 mb-5"></i>
+              <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Quality</h1>
+              <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
+                We only work with the best manufacturers and designers in China
+                to ensure that our customers receive the highest quality
+                products.
+              </p>
+            </div>
+            <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/3">
+              <i class="fad fa-dollar-sign text-6xl text-green-500 mb-5"></i>
+              <h1 class="text-2xl font-bold mb-3 mt-5 text-white">
+                Affordability
+              </h1>
+              <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
+                We work directly with manufacturers and designers to cut out the
+                middleman and provide our customers with the best prices.
+              </p>
+            </div>
+            <div class="flex flex-col items-center justify-center md:w-1/3">
+              <i class="fad fa-headset text-6xl text-green-500 mb-5"></i>
+              <h1 class="text-2xl font-bold mb-3 mt-5 text-white">
+                Customer Service
+              </h1>
+              <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
+                We are dedicated to providing our customers with the best
+                service possible. If you have any questions or concerns, please
+                contact us at
+                <a href="mailto:info@dripplug.io" class="text-green-500">
+                  info@dripplug.io
+                </a>
+                .
+              </p>
+            </div>
+          </div>
         </div>
       </section>
       <section>
-        <div className="flex flex-col items-center justify-center w-full py-10 text-center bg-white"
-        >
+        <div className="flex flex-col items-center justify-center w-full py-10 text-center bg-white">
           <h1 className="text-4xl font-bold mb-3 mt-5 text-black">Our Team</h1>
 
           <div className="flex flex-row items-center justify-center w-full py-10 text-center">
-            <div className="flex flex-col items-center justify-center w-1/3">
-
+            <div className="flex flex-col items-center justify-center lg:w-1/3">
               <Image
-
                 src="/profile.png"
                 alt="Drip Plug Logo"
                 width={300}
@@ -98,29 +106,35 @@ export default function Home() {
         </div>
       </section>
       <section>
-  <div class="flex flex-col items-center justify-center w-full py-10 text-center bg-black">
-    <h1 class="text-4xl font-bold mb-3 mt-5 text-white">Contact Us</h1>
+        <div class="flex flex-col items-center justify-center w-full py-10 text-center bg-black">
+          <h1 class="text-4xl font-bold mb-3 mt-5 text-white">Contact Us</h1>
 
-    <div class="flex flex-col md:flex-row items-center justify-center w-full py-10 text-center">
-      <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/2">
-        <i class="fad fa-envelope text-6xl text-green-500 mb-5"></i>
-        <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Email</h1>
-        <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
-          If you have any questions or concerns, please contact us at
-          <a href="mailto:info@dripplug.io" class="text-green-500 ml-1">info@dripplug.io</a>.
-        </p>
-      </div>
-      <div class="flex flex-col items-center justify-center md:w-1/2">
-        <i class="fad fa-map-marker-alt text-6xl text-green-500 mb-5"></i>
-        <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Address</h1>
-        <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
-          If you have any questions or concerns, please contact us at
-          <span class="text-green-500 ml-1">105 E Maplewood St Walsh, Colorado(CO), 81090 📍</span>.
-        </p>
-      </div>
-    </div>
-  </div>
-</section>
+          <div class="flex flex-col md:flex-row items-center justify-center w-full py-10 text-center">
+            <div class="flex flex-col items-center justify-center mb-10 md:mb-0 md:w-1/2">
+              <i class="fad fa-envelope text-6xl text-green-500 mb-5"></i>
+              <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Email</h1>
+              <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
+                If you have any questions or concerns, please contact us at
+                <a href="mailto:info@dripplug.io" class="text-green-500 ml-1">
+                  info@dripplug.io
+                </a>
+                .
+              </p>
+            </div>
+            <div class="flex flex-col items-center justify-center md:w-1/2">
+              <i class="fad fa-map-marker-alt text-6xl text-green-500 mb-5"></i>
+              <h1 class="text-2xl font-bold mb-3 mt-5 text-white">Address</h1>
+              <p class="text-xl text-gray-400 w-4/5 md:w-1/2 mb-10">
+                If you have any questions or concerns, please contact us at
+                <span class="text-green-500 ml-1">
+                  105 E Maplewood St Walsh, Colorado(CO), 81090 📍
+                </span>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -7,7 +7,6 @@ import { useEffect } from "react";
 
 // import QCDropDown component from components/qcdropdown.tsx
 
-
 export default function Product({ params }) {
   const [product, setProduct] = useState(null);
   const [images, setImages] = useState(null);
@@ -16,23 +15,17 @@ export default function Product({ params }) {
   let image;
   let goodsId;
 
-  if(!images) {
-    
+  if (!images) {
     image = "";
-    
-
   } else {
-    goodsId = images[0][0]['goodsId'];
+    goodsId = images[0][0]["goodsId"];
     image = images[1];
   }
-
 
   useEffect(() => {
     getProductById(params.id)
       .then((data) => setProduct(data))
-      .catch((error) => {
-
-      });
+      .catch((error) => {});
   }, [params.id]);
 
   useEffect(() => {
@@ -49,7 +42,6 @@ export default function Product({ params }) {
     }
   }, [product]);
 
-
   // getQcImagesRequest using goodsId
 
   useEffect(() => {
@@ -62,191 +54,221 @@ export default function Product({ params }) {
     }
   }, [goodsId]);
 
-
   // set qcImages to qcImages[0]['photos'] using setQcImages
 
   if (qcImages) {
+    // qcImages = qcImages[0]['photos'];
 
-    qcImages = qcImages[0]['photos'];
+    const combinedPhotos = qcImages.reduce(
+      (accumulator, item) => accumulator.concat(item.photos),
+      []
+    );
+
+    qcImages = combinedPhotos;
+
+    console.log(combinedPhotos);
   }
-
-        
-
-
-
-
 
   if (product && !product?.name) {
-    return <div className="p-5 text-center h-screen flex">
-        <span className="my-auto mx-auto text-3xl">Could not resolve product</span>
-        </div>;
+    return (
+      <div className="p-5 text-center h-screen flex">
+        <span className="my-auto mx-auto text-3xl">
+          Could not resolve product
+        </span>
+      </div>
+    );
   }
-
-
-
-  
 
   // convert product price chinese yuan to usd
 
   const convertPrice = (price) => {
     // turn string to number and remove uneccessary characters
-    if(price == undefined) {
+    if (price == undefined) {
       return 0;
     }
-    price = Number(price.replace(/[^0-9.-]+/g,""));
-    return (price * 0.15);
-  }
-  
+    price = Number(price.replace(/[^0-9.-]+/g, ""));
+    return price * 0.15;
+  };
+
   return (
-<main
-style={{
-  // linear gradient gray to light gray
-  background: "linear-gradient(180deg, #222 0%, #22C55E 500%)",
-}}
->
-  <div className="container lg:py-10 lg:px-10 mx-auto">
-    {product ? (
-    <div className="min-h-screen shadow-xl rounded-md"
-    style={{ 
-      // linear gradient gray to light gray
-      background: "rgba(35, 35, 35, 0.5)",
-     }}
-    >
-    <div className="max-w-7xl mx-auto lg:py-12 lg:px-4 sm:px-6 lg:px-8">
-      <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-        {/* Product Image */}
-        <div className="lg:col-span-7">
-  {image ? (
-    <img
-      className="w-full h-auto lg:rounded-md lg:shadow-lg shadow-md p-5"
-      src={image}
-      alt="Product Image"
+    <main
       style={{
-        background: "linear-gradient(360deg, rgba(45,45,45,0.8) 0%, rgba(34,197,94,0.3) 150%)",
+        // linear gradient gray to light gray
+        background: "linear-gradient(180deg, #222 0%, #22C55E 500%)",
       }}
-    />
-  ) : (
-    <img src="/spinner.svg" alt="" />
-  )}
-</div>
-        {/* Product Details */}
-        <div className="lg:col-span-5 mt-8 lg:mt-0 flex flex-col justify-between
+    >
+      <div className="container lg:py-10 lg:px-10 mx-auto">
+        {product ? (
+          <div
+            className="min-h-screen shadow-xl rounded-md"
+            style={{
+              // linear gradient gray to light gray
+              background: "rgba(35, 35, 35, 0.5)",
+            }}
+          >
+            <div className="max-w-7xl mx-auto lg:py-12 lg:px-4 sm:px-6 lg:px-8">
+              <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                {/* Product Image */}
+                <div className="lg:col-span-7">
+                  {image ? (
+                    <img
+                      className="w-full h-auto lg:rounded-md lg:shadow-lg shadow-md p-5"
+                      src={image}
+                      alt="Product Image"
+                      style={{
+                        background:
+                          "linear-gradient(360deg, rgba(45,45,45,0.8) 0%, rgba(34,197,94,0.3) 150%)",
+                      }}
+                    />
+                  ) : (
+                    <img src="/spinner.svg" alt="" />
+                  )}
+                </div>
+                {/* Product Details */}
+                <div
+                  className="lg:col-span-5 mt-8 lg:mt-0 flex flex-col justify-between
         px-4 lg:px-0
         py-4 lg:py-20
-        ">
-          <div className="info">
-          <h1 className="text-3xl font-semibold mb-4">
-            {
-              product.name
-            }
-          </h1>
-          {/* views */}
-          <i className="fas fa-eye text-gray-400 mb-4"></i>
-          <span className="text-gray-400 mb-4"> 1000 views</span>
-          
-          <div className="
+        "
+                >
+                  <div className="info">
+                    <h1 className="text-3xl font-semibold mb-4">
+                      {product.name}
+                    </h1>
+                    {/* views */}
+                    <i className="fas fa-eye text-gray-400 mb-4"></i>
+                    <span className="text-gray-400 mb-4"> 1000 views</span>
+
+                    <div
+                      className="
           ml-5
-          ">
+          "
+                    >
+                      {/* stars */}
+                      <i className="fas fa-star text-yellow-400 mb-4"></i>
+                      <i className="fas fa-star text-yellow-400 mb-4"></i>
+                      <i className="fas fa-star text-yellow-400 mb-4"></i>
+                      <i className="fas fa-star text-yellow-400 mb-4"></i>
+                    </div>
 
-          {/* stars */}
-          <i className="fas fa-star text-yellow-400 mb-4"></i>
-          <i className="fas fa-star text-yellow-400 mb-4"></i>
-          <i className="fas fa-star text-yellow-400 mb-4"></i>
-          <i className="fas fa-star text-yellow-400 mb-4"></i>
-
-          </div>
-
-
-          <p className="text-md text-gray-400 mb-6 w-4/5">
-            
-              
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium eum optio dolorem nihil molestias repudiandae. Ea itaque minima neque numquam voluptas quibusdam magni ut laudantium culpa, blanditiis facilis consequatur reprehenderit?
-              
-            
-          </p>
-          </div>
-          <div className="actions">
-          <div className="flex items-center mb-4 justify-between">
-            <span className="text-xl font-semibold mr-2"
-            style={{ 
-              borderLeft: "6px solid #22C55E",
-              backgroundColor: "rgba(34, 197, 94, 0.1)",
-              padding: "0.5rem 1rem",
-              borderRadius: "0.2rem",
-              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)"
-             }}
-            >
-              {
-                "$" + convertPrice(product.price).toFixed(2)
-              }
-            </span>
-            <span className="text-gray-300">Regular Price: {
-              "$" + convertPrice(product.price).toFixed(2) * 8
-            }</span>
-          </div>
-            <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600
+                    <p className="text-md text-gray-400 mb-6 w-4/5">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Accusantium eum optio dolorem nihil molestias repudiandae.
+                      Ea itaque minima neque numquam voluptas quibusdam magni ut
+                      laudantium culpa, blanditiis facilis consequatur
+                      reprehenderit?
+                    </p>
+                  </div>
+                  <div className="actions">
+                    <div className="flex items-center mb-4 justify-between">
+                      <span
+                        className="text-xl font-semibold mr-2"
+                        style={{
+                          borderLeft: "6px solid #22C55E",
+                          backgroundColor: "rgba(34, 197, 94, 0.1)",
+                          padding: "0.5rem 1rem",
+                          borderRadius: "0.2rem",
+                          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                        }}
+                      >
+                        {"$" + convertPrice(product.price).toFixed(2)}
+                      </span>
+                      <span className="text-gray-300">
+                        Regular Price:{" "}
+                        {"$" + convertPrice(product.price).toFixed(2) * 8}
+                      </span>
+                    </div>
+                    <button
+                      className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600
             transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-green w-full
             shadow-md
-            ">Add to Cart</button>
+            "
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="p-10 pl-0"
+              style={{
+                background: "rgba(35, 35, 35, 0.7)",
+                borderTop: "3px solid #22C55E",
+              }}
+            >
+              {/* quality check image section */}
+              <h1 className="text-4xl font-semibold ml-5 lg:ml-20 mb-6 mt-8 text-left">
+                Quality Check Images
+              </h1>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4 lg:px-20">
+                {Array.isArray(qcImages) ? (
+                  qcImages.map((image, index) => (
+                    <div
+                      key={index}
+                      className="flex items-center justify-center" // Center the content horizontally and vertically
+                    >
+                      <img
+                        src={image.src}
+                        alt=""
+                        className="w-full h-auto rounded-md shadow-md"
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <img src="/spinner.svg" alt="" />
+                )}
+              </div>
+            </div>
           </div>
-        </div>
+        ) : (
+          <img src="/spinner.svg" alt="" />
+        )}
       </div>
-    </div>
-
-    <div className="p-10 pl-0"
-    style={
-      {
-        background: "rgba(35, 35, 35, 0.7)",
-        borderTop: "3px solid #22C55E",
-      }
-    }
-    >
-    {/* quality check image section */}
-    <h1
-    className="text-4xl font-semibold ml-5 lg:ml-20 mb-6 mt-8 text-left"
-    >
-      Quality Check Images
-    </h1>
-
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4 lg:px-20">
-    
-    {
-      Array.isArray(qcImages) ? (
-        qcImages.map((image, index) => (
-          <img src={image.src} alt="" key={index} className="w-full h-auto rounded-md shadow-md" />
-        ))
-      ) : (
-        <img src="/spinner.svg" alt="" />
-      )
-    }
-
-    </div>
-
-  </div>
-  </div>
-    ) : (
-      <img src="/spinner.svg" alt="" />
-    )}
-  </div>
-</main>
+    </main>
   );
 }
 
 const getProductById = async (id) => {
-  const res = await fetch(`http://localhost:9000/product/${id}`);
+  const res = await fetch(`http://192.168.1.14:9000/product/${id}`);
   const data = await res.json();
   return data;
 };
 
 const getImages = async (link) => {
   // get only after https://pandabuy.page.link/
-  const res = await fetch(`http://localhost:9000/qc/${link}?all=true`);
+  const res = await fetch(`http://192.168.1.14:9000/qc/${link}?all=true`);
   const data = await res.json();
   return data;
 };
 
 const getQcImagesRequest = async (goodsId) => {
-  const res = await fetch(`http://localhost:9000/getqc/${goodsId}`);
+  const res = await fetch(`http://192.168.1.14:9000/getqc/${goodsId}`);
   const data = await res.json();
   return data;
+};
+
+const useFetch = (url:string) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
 }
